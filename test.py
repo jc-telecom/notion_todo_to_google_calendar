07e65a4535f11d913e0c9acd29e8dda636001deb
Jules <crouzton3@gmail.com>
2024-01-24 17:46:33 +0100

    updated readme

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
 import json
-print(json.loads("false"))
+
+# Your JSON data
+json_data = '''
+{
+  "checkbox": { "path": ["checkbox"] },
+  "date": { "path": ["date"] },
+  "email": { "path": ["email"] },
+  "last_edited_by": { "path": ["id"], "method": "get_user_name" },
+  "multi_select": { "path": ["multi_select", "name"] },
+  "number": { "path": ["number"] },
+  "people": { "path": ["people", "id"], "method": "get_user_name" },
+  "phone_number": { "path": ["phone_number"] },
+  "rich_text": { "path": ["rich_text", "plain_text"] },
+  "select": { "path": ["select", "name"] },
+  "status": { "path": ["status", "name"] },
+  "title": { "path": ["title", "plain_text"] },
+  "url": { "path": ["url"] },
+  "relation": { "path": ["relation", "id"], "method": "get_page_name" }
+}
+'''
+
+# Parse the JSON data
+parsed_data = json.loads(json_data)
+
+# Get the keys as a list
+keys_list = list(parsed_data.keys())
+
+# Print the keys
+print(keys_list)
